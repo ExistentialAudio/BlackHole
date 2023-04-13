4d0f9f6291f0753b83b72e03ec7a90fa13e9ae21
Steffan Andrews <stef@orchetect.com>
2023-04-12 23:40:47 -0700

    Installer script cleanup

--- Installer/create_installer.sh
+++ Installer/create_installer.sh
@@ -1,83 +1,119 @@
 #!/usr/bin/env bash
 
-# run for BlackHole directory.
+# Creates installer for different channel versions.
+# Run this script from the local BlackHole repo's root directory.
 
-# create installer for different channel versions
+devTeamID="Q5C99V536K" # ⚠️ Replace this with your own developer team ID
+notarize=true # To skip notarization, set this to false
+notarizeProfile="notarize" # ⚠️ Replace this with your own notarytool keychain profile name
 
-for channels in 2 16 64 128 256
-do
+############################################################################
 
-ch=$channels"ch"
-driverName="BlackHole"
-version=`git describe --tags --abbrev=0`
-bundleID="audio.existential.$driverName$ch"
+# Basic Validation
+if [ ! -d BlackHole.xcodeproj ]; then
+    echo "This script must be run from the BlackHole repo root folder."
+    echo "For example:"
+    echo "  cd /path/to/BlackHole"
+    echo "  ./Installer/create_installer.sh"
+    exit 1
+fi
 
-# Build
-xcodebuild \
--project BlackHole.xcodeproj \
--configuration Release \
--target BlackHole CONFIGURATION_BUILD_DIR=build \
-PRODUCT_BUNDLE_IDENTIFIER=$bundleID \
-GCC_PREPROCESSOR_DEFINITIONS='$GCC_PREPROCESSOR_DEFINITIONS 
-kNumber_Of_Channels='$channels' 
-kPlugIn_BundleID=\"'$bundleID'\" 
-kDriver_Name=\"'$driverName'\"'
-
-# Generate a new UUID
-uuid=$(uuidgen)
-awk '{sub(/e395c745-4eea-4d94-bb92-46224221047c/,"'$uuid'")}1' build/BlackHole.driver/Contents/Info.plist > Temp.plist
-mv Temp.plist build/BlackHole.driver/Contents/Info.plist
-
-mkdir installer/root
-mv build/BlackHole.driver installer/root/$driverName$ch.driver
-rm -r build
-
-# Sign
-codesign --force --deep --options runtime --sign Q5C99V536K Installer/root/$driverName$ch.driver
-
-# Create package with pkgbuild
-chmod 755 Installer/Scripts/preinstall
-chmod 755 Installer/Scripts/postinstall
-
-pkgbuild --sign "Q5C99V536K" --root Installer/root --scripts Installer/Scripts --install-location /Library/Audio/Plug-Ins/HAL Installer/BlackHole.pkg
-rm -r Installer/root
-
-# Create installer with productbuild
-cd Installer
-
-echo "<?xml version=\"1.0\" encoding='utf-8'?>
-<installer-gui-script minSpecVersion='2'>
-    <title>$driverName: Virtual Audio Driver $ch $version</title>
-    <welcome file='welcome.html'/>
-    <license file='../LICENSE'/>
-    <conclusion file='conclusion.html'/>
-    <domains enable_anywhere='false' enable_currentUserHome='false' enable_localSystem='true'/>
-    <pkg-ref id=\"$bundleID\"/>
-    <options customize='never' require-scripts='false' hostArchitectures='x86_64,arm64'/>
-    <volume-check>
-        <allowed-os-versions>
-            <os-version min='10.9'/>
-        </allowed-os-versions>
-    </volume-check>
-    <choices-outline>
-        <line choice=\"$bundleID\"/>
-    </choices-outline>
-    <choice id=\"$bundleID\" visible='true' title=\"$driverName $ch\" start_selected='true'>
+for channels in 2 16 64 128 256; do
+    # Env
+    ch=$channels"ch"
+    driverName="BlackHole"
+    version=`git describe --tags --abbrev=0`
+    driverVartiantName=$driverName$ch
+    bundleID="audio.existential.$driverVartiantName"
+    
+    # Build
+    xcodebuild \
+      -project BlackHole.xcodeproj \
+      -configuration Release \
+      -target BlackHole CONFIGURATION_BUILD_DIR=build \
+      PRODUCT_BUNDLE_IDENTIFIER=$bundleID \
+      GCC_PREPROCESSOR_DEFINITIONS='$GCC_PREPROCESSOR_DEFINITIONS 
+      kNumber_Of_Channels='$channels' 
+      kPlugIn_BundleID=\"'$bundleID'\" 
+      kDriver_Name=\"'$driverName'\"'
+    
+    # Generate a new UUID
+    uuid=$(uuidgen)
+    awk '{sub(/e395c745-4eea-4d94-bb92-46224221047c/,"'$uuid'")}1' build/BlackHole.driver/Contents/Info.plist > Temp.plist
+    mv Temp.plist build/BlackHole.driver/Contents/Info.plist
+    
+    mkdir Installer/root
+    driverBundleName=$driverVartiantName.driver
+    mv build/BlackHole.driver Installer/root/$driverBundleName
+    rm -r build
+    
+    # Sign
+    codesign \
+      --force \
+      --deep \
+      --options runtime \
+      --sign $devTeamID \
+      Installer/root/$driverBundleName
+    
+    # Create package with pkgbuild
+    chmod 755 Installer/Scripts/preinstall
+    chmod 755 Installer/Scripts/postinstall
+    
+    pkgbuild \
+      --sign $devTeamID \
+      --root Installer/root \
+      --scripts Installer/Scripts \
+      --install-location /Library/Audio/Plug-Ins/HAL \
+      Installer/BlackHole.pkg
+    rm -r Installer/root
+    
+    # Create installer with productbuild
+    cd Installer
+    
+    echo "<?xml version=\"1.0\" encoding='utf-8'?>
+    <installer-gui-script minSpecVersion='2'>
+        <title>$driverName: Virtual Audio Driver $ch $version</title>
+        <welcome file='welcome.html'/>
+        <license file='../LICENSE'/>
+        <conclusion file='conclusion.html'/>
+        <domains enable_anywhere='false' enable_currentUserHome='false' enable_localSystem='true'/>
         <pkg-ref id=\"$bundleID\"/>
-    </choice>
-    <pkg-ref id=\"$bundleID\" version=\"$version\" onConclusion='none'>BlackHole.pkg</pkg-ref>
-</installer-gui-script>" >> distribution.xml
+        <options customize='never' require-scripts='false' hostArchitectures='x86_64,arm64'/>
+        <volume-check>
+            <allowed-os-versions>
+                <os-version min='10.9'/>
+            </allowed-os-versions>
+        </volume-check>
+        <choices-outline>
+            <line choice=\"$bundleID\"/>
+        </choices-outline>
+        <choice id=\"$bundleID\" visible='true' title=\"$driverName $ch\" start_selected='true'>
+            <pkg-ref id=\"$bundleID\"/>
+        </choice>
+        <pkg-ref id=\"$bundleID\" version=\"$version\" onConclusion='none'>BlackHole.pkg</pkg-ref>
+    </installer-gui-script>" >> distribution.xml
+    
+    # Build
+    installerPkgName="$driverVartiantName.$version.pkg"
+    productbuild \
+      --sign $devTeamID \
+      --distribution distribution.xml \
+      --resources . \
+      --package-path BlackHole.pkg $installerPkgName
+    rm distribution.xml
+    rm -f BlackHole.pkg
+    
+    # Notarize and Staple
+    if [ "$notarize" = true ]; then
+        xcrun \
+          notarytool submit $installerPkgName \
+          --team-id $devTeamID \
+          --progress \
+          --wait \
+          --keychain-profile $notarizeProfile
+        
+        xcrun stapler staple $installerPkgName
+    fi
 
-
-productbuild --sign "Q5C99V536K" --distribution distribution.xml --resources . --package-path BlackHole.pkg $driverName$ch.$version.pkg
-rm distribution.xml
-rm -f BlackHole.pkg
-
-# Notarize
-xcrun notarytool submit $driverName$ch.$version.pkg --team-id Q5C99V536K --progress --wait --keychain-profile "notarize"
-
-xcrun stapler staple $driverName$ch.$version.pkg
-
-cd ..
-
+    cd ..
 done
