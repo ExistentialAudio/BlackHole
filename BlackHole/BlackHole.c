8497ee78ecfc00375bee5b8883ac7c67eb961877
Devin Roth <devin@devinrothmusic.com>
2025-05-26 15:54:39 -0700

    Add 24k audio.

--- BlackHole/BlackHole.c
+++ BlackHole/BlackHole.c
@@ -321,7 +321,7 @@ static const UInt32                 kDevice_ObjectListSize              = sizeof
 static const UInt32                 kDevice2_ObjectListSize              = sizeof(kDevice2_ObjectList) / sizeof(struct ObjectInfo);
 
 #ifndef kSampleRates
-#define                             kSampleRates       8000, 16000, 44100, 48000, 88200, 96000, 176400, 192000, 352800, 384000, 705600, 768000
+#define                             kSampleRates       8000, 16000, 24000, 44100, 48000, 88200, 96000, 176400, 192000, 352800, 384000, 705600, 768000
 #endif
 
 static Float64                      kDevice_SampleRates[]               = { kSampleRates };
